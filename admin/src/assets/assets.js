e178788901392f6a017b05a53d082c25cbcf2019
Md Kashif Mukhtar <81863352+kashiif373@users.noreply.github.com>
2025-09-17 13:11:38 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://food-online-delivery-system-4.onrender.com'
+
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +16,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:8080'
+//export const url = 'http://localhost:8080'
